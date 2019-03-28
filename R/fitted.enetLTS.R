e377451b17c4e0011769500daa22880b8819766e
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-03-28 15:15:41 +0100

    changed some identation

--- R/fitted.enetLTS.R
+++ R/fitted.enetLTS.R
@@ -2,9 +2,11 @@ fitted.enetLTS <- function(object, vers = c("reweighted","raw","both"), type = c
       vers <- match.arg(vers)
       type <- match.arg(type)
 
-      if(type=="class" & object$inputs$family!="binomial"){stop("'class' is only available for logistic regression")}
+      if(type=="class" & object$inputs$family != "binomial"){
+         stop("'class' is only available for logistic regression")
+         }
 
-      if (object$inputs$intercept==TRUE){
+      if (object$inputs$intercept == TRUE){
          reweighted.coefficients <- c(object$a0,object$coefficients)
          raw.coefficients <- c(object$a00,object$raw.coefficients)
       } else {
@@ -12,29 +14,29 @@ fitted.enetLTS <- function(object, vers = c("reweighted","raw","both"), type = c
          raw.coefficients <- c(object$a00,object$raw.coefficients)
       }
 
-      if (object$inputs$family=="binomial"){
-         if (vers=="reweighted"){
+      if (object$inputs$family == "binomial"){
+         if (vers == "reweighted"){
             u <- object$inputs$x %*% reweighted.coefficients
-            if (type=="class"){
+            if (type == "class"){
                fitted.values <- ifelse(u>0.5,0,1)
-            } else if (type=="response"){
+            } else if (type == "response"){
                fitted.values <-  1/(1+exp(-u))
             }
             nfit <- list(fitted.values=fitted.values)
-         } else if (vers=="raw"){
+         } else if (vers == "raw"){
             uu <- object$inputs$x %*% raw.coefficients
-            if (type=="class"){
+            if (type == "class"){
                raw.fitted.values <- ifelse(uu>0.5,0,1)
-            } else if (type=="response"){
+            } else if (type == "response"){
                raw.fitted.values <-  1/(1+exp(-uu))
             }
-            nfit <- list(raw.fitted.values=raw.fitted.values)
-         } else if (vers=="both"){
+            nfit <- list(raw.fitted.values = raw.fitted.values)
+         } else if (vers == "both"){
             u <- object$inputs$x %*% reweighted.coefficients
             uu <- object$inputs$x %*% reweighted.coefficients
-            if (type=="class"){
+            if (type == "class"){
                fitted.values <- ifelse(u>0.5,0,1)
-            } else if (type=="response"){
+            } else if (type == "response"){
                fitted.values <- 1/(1+exp(-u))
             }
             if (type=="class"){
