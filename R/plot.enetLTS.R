78f893c46bfb7b01e5778c0bfe46ae9f72518c83
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-03-28 14:42:56 +0100

    cleaned identation

--- R/plot.enetLTS.R
+++ R/plot.enetLTS.R
@@ -1,21 +1,17 @@
-
-
-plot.enetLTS <- function(x,method=c("coefficients","resid","diagnostic"),
-                         vers=c("reweighted","raw"),...){
+plot.enetLTS <- function(x, method = c("coefficients", "resid", "diagnostic"),
+                         vers = c("reweighted", "raw"), ...){
    #  require(ggplot2)
    
    vers <- match.arg(vers)
-   par(ask=T)
+   par(ask = TRUE)
    readline(prompt = "Press <Enter> to continue...")
    ## call plot function
- 
       if (missing("method")){
-         plotCoef.enetLTS(x,vers=vers,...)
-         plotResid.enetLTS(x,vers=vers,...)
-         plotDiagnostic.enetLTS(x,vers=vers,...) }
-      else if(method == "coefficients") plotCoef.enetLTS(x,vers=vers,...)
-      else if(method == "resid") plotResid.enetLTS(x,vers=vers,...)
-      else if(method == "diagnostic") plotDiagnostic.enetLTS(x,vers=vers,...)
-   
+         plotCoef.enetLTS(x, vers = vers, ...)
+         plotResid.enetLTS(x, vers = vers, ...)
+         plotDiagnostic.enetLTS(x, vers = vers, ...) }
+      else if(method == "coefficients") plotCoef.enetLTS(x, vers = vers, ...)
+      else if(method == "resid") plotResid.enetLTS(x, vers = vers, ...)
+      else if(method == "diagnostic") plotDiagnostic.enetLTS(x, vers = vers, ...)
 }
 
