3e2e37b1132f5e98c2408fbc5bfe2be8ffb3b00d
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-03-28 15:19:42 +0100

    separated weight.binomial() from prepara.R function

---
+++ R/weight.binomial.R
@@ -0,0 +1,11 @@
+weight.binomial <- function(x, y, beta, intercept, del){
+  if(intercept == TRUE){
+    pi <- exp(cbind(1,x) %*% beta)/(1 + exp(cbind(1, x) %*% beta))
+    res <- (y - pi) / sqrt(pi*(1-pi))
+  } else{
+    pi <- exp(x %*% beta[-1])/(1 + exp(x %*% beta[-1]))
+    res <- (y - pi) / sqrt(pi * (1 - pi))
+  }
+  we <- as.integer(abs(res) <= qnorm(1 - del))
+  return(we)
+}
