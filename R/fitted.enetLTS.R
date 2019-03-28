9401a5fc1e72f32d4662722eb14db2718c7c01e5
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-03-28 15:16:31 +0100

    changed some identation

--- R/fitted.enetLTS.R
+++ R/fitted.enetLTS.R
@@ -39,23 +39,23 @@ fitted.enetLTS <- function(object, vers = c("reweighted","raw","both"), type = c
             } else if (type == "response"){
                fitted.values <- 1/(1+exp(-u))
             }
-            if (type=="class"){
+            if (type == "class"){
                raw.fitted.values <- ifelse(uu>0.5,0,1)
-            } else if (type=="response"){
+            } else if (type == "response"){
                raw.fitted.values <- 1/(1+exp(-uu))
             }
             nfit <- list(fitted.values=fitted.values,raw.fitted.values=raw.fitted.values)
          }
-      } else if (object$inputs$family=="gaussian"){
-         if (vers=="reweighted"){
+      } else if (object$inputs$family == "gaussian"){
+         if (vers == "reweighted"){
             res=as.matrix(object$inputs$x %*% reweighted.coefficients)
             nfit <- list(fitted.values=res)
-         } else if (vers=="raw"){
-            res=as.matrix(object$inputs$x %*% raw.coefficients)
+         } else if (vers == "raw"){
+            res <- as.matrix(object$inputs$x %*% raw.coefficients)
             nfit <- list(raw.fitted.values=res)
-         } else if (vers=="both"){
-            res1=as.matrix(object$inputs$x %*% reweighted.coefficients)
-            res2=as.matrix(object$inputs$x %*% raw.coefficients)
+         } else if (vers == "both"){
+            res1 <- as.matrix(object$inputs$x %*% reweighted.coefficients)
+            res2 <- as.matrix(object$inputs$x %*% raw.coefficients)
             nfit <- list(fitted.values=res1,raw.fitted.values=res2)
 
          }
