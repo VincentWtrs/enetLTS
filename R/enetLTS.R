b74e0910d4916430aa938bac881d611afa0f2993
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-03-29 14:33:02 +0100

    added comments, mostly sourced from the _ANNOTATED versions I made in the past + moved IC checks more upfront

--- R/enetLTS.R
+++ R/enetLTS.R
@@ -1,87 +1,143 @@
 enetLTS <- function(xx, yy, family = c("gaussian", "binomial"), alphas, 
-                   lambdas, lambdaw, hsize = 0.75, intercept = TRUE, nsamp = 500, 
-                   s1 = 10, nCsteps = 20, nfold = 5, seed = NULL, plot = TRUE, 
-                   repl = 5, para = TRUE, ncores = 1, del = 0.0125, tol = -1e+06, 
-                   scal = TRUE, type = c("response", "class"), ic_type = NULL, type_lambdaw = "min"){
+                    lambdas, lambdaw, hsize = 0.75, intercept = TRUE, nsamp = 500, 
+                    s1 = 10, nCsteps = 20, nfold = 5, seed = NULL, plot = TRUE, 
+                    repl = 5, para = TRUE, ncores = 1, del = 0.0125, tol = -1e+06, 
+                    scal = TRUE, type = c("response", "class"), ic_type = NULL, type_lambdaw = "min"){
   
   #### UPDATED VERSION ###
   # NEW: ic_type test test
   # NEW: type_lambdaw: choosing lambda.min or lambda.1se for the reweighting step.
   
+  ## INPUT
+  # xx: Predictor matrix (numeric, hence dummies need to be constructed beforehand)
+  # yy: Outcome variable, for binomial factor CODED as 0/1!
+  # family: The outcome variable distribution family (R Documentation states error distribution, this is wrong)
+  # alphas: Sequence of alphas [0, 1] to fit the models, doesn't have to be in any order.
+  # lambdas: Positive sequence of lambdas to fit the models, doesn't have to be in any order.
+  # lamdbdaw: Positive sequence of lambdas for the reweighted fits
+  # hsize: Fraction of data to be used for fitting. Default: 0.75, pick lower for more conservative fitting
+  # intercept: Logical if intercept needs to be included. Best to use default (TRUE), and keep intercept out of xx!
+  # scal: Logical if scaling of the h-subsamples needs to be performed, default: TRUE.
+  # nsamp: The amount of elemental subsets (size = 3 or 4) to generate, default: 500.
+  # s1: The amount of promising h (?) subsets to keep after running 2 C-steps on the nsamp elemental subsets.
+  # nCsteps: A maximum amount of Csteps that the while-loops have to go through (to prevent stuck while-loops, default: 20).
+  # nfold: Amount of folds to be used in the k-fold cross validation, default: 5.
+  # seed: Initial seed for the random number generator for determining elemental subsets.
+  # plot: Logical, if the alpha-lambda-Error tile plot needs to be made, default: TRUE.
+  # repl: Amount of replications (repetitions) of the cross validation procedure, default: 5.
+  # para: parallel fitting of the folds in the cross validation structure.
+  # ncores: For parallel computation of certain parts (different than the CV structure!). NOT AVAILABLE ON WINDOWS.
+  # del: 1-del: is the quantile to give 0-weight to an observation (hence this is flagged as outlier) in the reweighing step!
+  # tol: Tolerance for stopping while loops in the C-step procedures, default: -1e+06
+  # type: type of predictions required, default: c("response", "class")
+  
   print("USING UPDATED VERSION OF enetLTS") # So I can see that the new function is effectively called
   ########################
   
-  matchedCall <- match.call()
+  matchedCall <- match.call() # Use match.call() if there are a lot of optional arguments
   matchedCall[[1]] <- as.name("enetLTS")
+  
   family <- match.arg(family)
   type <- match.arg(type)
-  xx <- addColnames(as.matrix(xx))
-  nc = dim(yy)
-  if (is.null(nc)) {
+  xx <- addColnames(as.matrix(xx)) # Adds column names
+  nc = dim(yy) # Dimension of outcome matrix (n x 1)
+  if (is.null(nc)) { # If yy is a vector, force it to matrix
     yy <- as.matrix(yy)
   }
-  n <- nrow(xx)
-  p <- ncol(xx)
-  h <- floor((n + 1) * hsize)
-  if (repl <= 0) 
+  
+  n <- nrow(xx) # Amount of observations
+  p <- ncol(xx) # Amount of TRUE predictors (EXCLUDING INTERCEPT)
+  h <- floor((n + 1) * hsize) # Size of outlier free set 
+  
+  ## Input parameters checks
+  # CV repetitions (require positive)
+  if (repl <= 0) {
     stop("repl has to be a positive number")
-  if (nCsteps <= 0) 
+  }
+  
+  # Max C-step amount (require positive)
+  if (nCsteps <= 0) {
     stop("nCsteps has to be a positive number")
-  if (type == "class" & family == "gaussian") 
+  }
+  
+  # Output/Family consistency (class requires binomial)
+  if (type == "class" & family == "gaussian") {
     stop("class type is not available for gaussian family")
+  }
+  
+  # CPU cores (requires valid and <= than available cores)
   ncores <- rep(ncores, length.out = 1)
-  if (is.na(ncores)) 
+  if (is.na(ncores)) {
     ncores <- detectCores()
-  if (!is.numeric(ncores) || is.infinite(ncores) || ncores < 1){
+  }
+  if (!is.numeric(ncores) || is.infinite(ncores) || ncores < 1) {
     ncores <- 1
     warning("invalid value of 'ncores'; using default value")
+  } else {
+    ncores <- as.integer(ncores)
   }
-  else ncores <- as.integer(ncores)
-  if (family == "binomial") {
-    rnames <- rownames(yy)
-    rownames(yy) <- 1:nrow(yy)
-    y_actual = as.factor(yy)
-    ntab = table(y_actual)
-    minclass = min(ntab)
-    classnames = names(ntab)
-  }
-  if (missing(alphas)) {
-    alphas <- seq(0, 1, length = 41)
-  }
-  alphas <- sort(alphas)
-  wh <- (alphas < 0 | alphas > 1)
-  if (sum(wh) > 0) 
-    stop("alphas can take the values only between 0 and 1")
-  if (missing(lambdas) & family == "gaussian") {
-    l0 <- robustHD::lambda0(xx, yy, normalize = scal, intercept = intercept)
-    lambdas <- seq(l0, 0, by = -0.025 * l0)
-  } else if (missing(lambdas) & family == "binomial") {
-    l00 <- lambda00(x = xx, 
-                    y = yy, 
-                    normalize = scal, 
-                    intercept = intercept)
-    lambdas <- seq(l00, 0, by = -0.025 * l00)
-  }
-  sc <- prepara(x = xx, 
-                y = yy, 
-                family = family, 
-                robu = 1)
-  x <- sc$xnor
-  y <- sc$ycen
   
-  # Information criterion check
+  # Information criterion (IC) (require nfold, repl to be 1)
   if(!is.null(ic_type)){
     print("Information criterion supplied: the cross-validation input parameters (nfold, repl) are ignored")
     nfold <- 1
     repl <- 1
   }
   
-  # Check if IC is used together with Gaussian family (not implemented yet)
+  # IC check with family (requires "binomial" currently)
   if(!is.null(ic_type)){
     if(family == "gaussian"){
       stop("Information criterion approach not yet supported for Gaussian family") # Some error throwing
     }
   }
+  
+  ## Binomial response preparation
+  if (family == "binomial") {
+    rnames <- rownames(yy) # Saving rownames (TO DO: why?)
+    rownames(yy) <- 1:nrow(yy) # Giving numbers as rownames
+    y_actual = as.factor(yy) # Converting to factor
+    ntab = table(y_actual) # Frequency table for y
+    minclass = min(ntab) # Saving minority outcome class
+    classnames = names(ntab) # Naming outcome class
+  }
+  
+  ## alphas sequence
+  # No given alphas: 41 evenly spaces values [0 ; 1]
+  if (missing(alphas)) {
+    alphas <- seq(from = 0, to = 1, length = 41)
+  }
+  
+  # If given (user-supplied) alphas:
+  alphas <- sort(alphas, decreasing = FALSE) # Increasing ordering
+  
+  wh <- (alphas < 0 | alphas > 1) # Checking if admissable
+  if (sum(wh) > 0) { # Bit hacky but works
+    stop("alphas can take the values only between 0 and 1")
+  }
+  
+  ## lambdas sequence
+  # Missing and Gaussian
+  if (missing(lambdas) & family == "gaussian") {
+    # Bivariate winsorization method
+    l0 <- robustHD::lambda0(xx, yy, normalize = scal, intercept = intercept)
+    lambdas <- seq(l0, 0, by = -0.025 * l0) # DECREASING SEQUENCE (HIGHEST REGULARIZATION FIRST) # TO DO: CHECK PROBABLY MORE LOGIC TO DO OTHER WAY AROUND TO GET OUTLIERS SINCE OTHERWISE THEY WILL BE HIDDEN IN THE SPAGHETTI (WARM START SEQUENCE) WHERE THE DIMENSIONALITY COLLAPSES AND THE OUTLIERS WILL NOT BE FOUND ANWAY...
+  } else if (missing(lambdas) & family == "binomial") { # Binomial case
+    # Point-biserial method
+    l00 <- lambda00(x = xx, 
+                    y = yy, 
+                    normalize = scal, 
+                    intercept = intercept)
+    lambdas <- seq(l00, 0, by = -0.025 * l00) # DECREASING SEQUENCE (HIGH REGULARIZATION FIRST) # TO DO: CHECK PROBABLY MORE LOGIC TO DO OTHER WAY AROUND TO GET OUTLIERS SINCE OTHERWISE THEY WILL BE HIDDEN IN THE SPAGHETTI (WARM START SEQUENCE) WHERE THE DIMENSIONALITY COLLAPSES AND THE OUTLIERS WILL NOT BE FOUND ANWAY...
+  }
+  
+  ## Data Preparation: robust centering and scaling
+  sc <- prepara(x = xx, 
+                y = yy, 
+                family = family, # If "binomial": y not touched!
+                robu = 1) # 
+  # Extracting normalized (x,y)
+  x <- sc$xnor # X is normalized (centering + scaling)
+  y <- sc$ycen # In "binomial" case: no change!
   
   ## C-STEPS
   WarmCstepresults <- warmCsteps(x = x, 
@@ -118,28 +174,28 @@ enetLTS <- function(xx, yy, family = c("gaussian", "binomial"), alphas,
   ## CROSS VALIDATION
   # Checking if grid is bigger than 1x1
   if((length(alphas) > 1) | (length(lambdas) > 1)){
-      # NEW: Changed from cv.enetLTS to cv.enetLTS_UPDATE ## NEW(2): got IC calculations out of cv.enetLTS_UPDATE
-      CVresults <- cv.enetLTS(index = indexall, 
-                              xx = x, 
-                              yy = y, 
-                              family = family, 
-                              h = h, 
-                              alphas = alphas, 
-                              lambdas = lambdas, 
-                              nfold = nfold, 
-                              repl = repl, 
-                              ncores = ncores, 
-                              plot = plot, 
-                              ic_type = ic_type) # NEW: ic_type ## NEW(2): remove ic_type # NEW(3) Think we can remove this due to default NULL
-      # Gathering results from CV
-      indexbest <- CVresults$indexbest
-      alphabest <- CVresults$alphaopt
-      lambdabest <- CVresults$lambdaopt
-      evalCritCV <- CVresults$evalCrit
+    # NEW: Changed from cv.enetLTS to cv.enetLTS_UPDATE ## NEW(2): got IC calculations out of cv.enetLTS_UPDATE
+    CVresults <- cv.enetLTS(index = indexall, 
+                            xx = x, 
+                            yy = y, 
+                            family = family, 
+                            h = h, 
+                            alphas = alphas, 
+                            lambdas = lambdas, 
+                            nfold = nfold, 
+                            repl = repl, 
+                            ncores = ncores, 
+                            plot = plot, 
+                            ic_type = ic_type) # NEW: ic_type ## NEW(2): remove ic_type # NEW(3) Think we can remove this due to default NULL
+    # Gathering results from CV
+    indexbest <- CVresults$indexbest
+    alphabest <- CVresults$alphaopt
+    lambdabest <- CVresults$lambdaopt
+    evalCritCV <- CVresults$evalCrit
   }
   
   # If scaling is TRUE
-  if (scal){
+  if(isTRUE(scal)){
     scl <- prepara(x = xx, 
                    y = yy, 
                    family = family, 
@@ -147,14 +203,16 @@ enetLTS <- function(xx, yy, family = c("gaussian", "binomial"), alphas,
                    robu = 0)
     xs <- scl$xnor
     ys <- scl$ycen
-    fit <- glmnet(x = xs[indexbest, ], 
-                  y = ys[indexbest, ], 
-                  family = family, 
-                  alpha = alphabest, 
-                  lambda = lambdabest, 
-                  standardize = FALSE, 
-                  intercept = FALSE) # TO DO: I THINK THIS NEEDS TO BE SPLITTED FOR BINOMIAL AND GAUSSIAN
-    if (family == "binomial"){
+    if(family == "gaussian"){ # New addition
+      fit <- glmnet(x = xs[indexbest, ], 
+                    y = ys[indexbest, ], 
+                    family = family, 
+                    alpha = alphabest, 
+                    lambda = lambdabest, 
+                    standardize = FALSE, 
+                    intercept = FALSE) # TO DO: I THINK THIS NEEDS TO BE SPLITTED FOR BINOMIAL AND GAUSSIAN
+    }
+    if(family == "binomial"){
       a00 <- if(intercept == FALSE) 
         0
       else drop(fit$a0 - as.vector(as.matrix(fit$beta)) %*% (scl$mux / scl$sigx))
@@ -182,7 +240,7 @@ enetLTS <- function(xx, yy, family = c("gaussian", "binomial"), alphas,
                              standardize = FALSE, 
                              intercept = FALSE, 
                              type.measure = "deviance")$lambda.1se # NEW: changed from $lambda.min to $lambda.1se AND type.measure = "mse" to "deviance"
-      
+        
       }
       else if (!missing(lambdaw) & length(lambdaw) == 1) {
         lambdaw <- lambdaw
