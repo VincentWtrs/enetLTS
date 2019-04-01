c09e20c1b0631f3b4e1e42c98e5579a32bdfd4b7
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-04-01 19:48:09 +0200

    some debugging code

--- R/enetLTS.R
+++ R/enetLTS.R
@@ -239,6 +239,9 @@ enetLTS <- function(xx, yy, family = c("gaussian", "binomial"), alphas,
       # Extracting raw results
       raw.coefficients <- drop(as.matrix(fit$beta) / scl$sigx) # TO DO Does this actually hold for logit??
       beta_with_int <- cbind(a00, as.matrix(fit$beta)) # NEW
+      # TO DO: remove
+      beta_with_int # TO REMOVE # DEBUGGING # TO DO
+      dim(cbind(1, xs)) # TO REMOVE # DEBUGGING # TO DO
       #raw.residuals <- -(ys * xs %*% as.matrix(fit$beta)) + log(1 + exp(xs %*% as.matrix(fit$beta))) # OLD
       raw.residuals <- -(ys * cbind(1, xs) %*% beta_with_int) + log(1 + exp(cbind(1, xs) %*% beta_with_int)) # NEW: cbind(1, xs) and beta_with_int
       raw.wt <- weight.binomial(x = xx, 
