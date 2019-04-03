242cea3be1ae5b993d47a9d19f1499236da39c20
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-04-03 19:45:41 +0200

    added some debugging code

--- R/CStep.R
+++ R/CStep.R
@@ -52,6 +52,7 @@ CStep <- function(x, y, family, indx, h, hsize, alpha, lambda, scal, new = TRUE)
         # TO DO: QUITE SURE THEY GET THE RESIDUALS ON N SAMPLES NOT ON xs[indx, ] SAMPLES HERE! (FIND OUT IF MAKES SENSE) (NEED TO LOOK UP C-STEPS)
         
         # Fallback if all beta == 0 # Stop early (?)
+        print("I am checking this now")
         print(beta)
         if (all(beta == 0)) {
           return(list(object = -Inf,index = indx, residu = resid, beta = beta))
