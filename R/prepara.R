f982020fdddf4f43db1612531e85e57d407ecb46
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-04-03 19:25:50 +0200

    cleaning code, layout

--- R/prepara.R
+++ R/prepara.R
@@ -1,48 +1,58 @@
 prepara <- function(x, y, family, index = NULL, robu = NULL){
   # default clasical scale, robust scale for robu=1
 
-  if (is.null(robu)) robu=0
+  if (is.null(robu)) robu <- 0
   if (is.null(index)) {
-    if (robu>0){
-      if(family=="binomial"){
-        muy=y
-      } else if(family=="gaussian"){
+    if (robu > 0){
+      if (family == "binomial") {
+        muy <- y
+      } else if (family == "gaussian") {
         muy <- median(y)
       }
-      mux <- apply(x,2,median)   ## to go back original coef
-      sigx <- apply(x,2,mad)
+      mux <- apply(x, 2, median)   ## to go back original coef
+      sigx <- apply(x, 2, mad)
     } else {
-      if(family=="binomial"){
-        muy=y
-      } else if(family=="gaussian"){
+      if(family == "binomial"){
+        muy <- y
+      } else if(family =="gaussian") {
         muy <- mean(y)
       }
-      mux <- apply(x,2,mean)   ## to go back original coef
-      sigx <- apply(x,2,sd)
+      mux <- apply(x, 2, mean)   ## to go back original coef
+      sigx <- apply(x, 2, sd)
     }
-  } else {
-    if (robu>0){
-      if(family=="binomial"){
-        muy=y
-      } else if(family=="gaussian"){
+  } else { # If index not NULL:
+    if (robu > 0){
+      if (family == "binomial") {
+        muy <- y
+      } else if (family == "gaussian") {
         muy <- median(y[index])
       }
-      mux <- apply(x[index,],2,median)
-      sigx <- apply(x[index,],2,mad)
+      mux <- apply(x[index, ], 2, median)
+      sigx <- apply(x[index, ], 2, mad)
     } else {
-      if(family=="binomial"){
-        muy=y
-      } else if(family=="gaussian"){
+      if (family == "binomial") {
+        muy <- y
+      } else if (family == "gaussian") {
         muy <- mean(y[index])
       }
-      mux <- apply(x[index,],2,mean)
-      sigx <- apply(x[index,],2,sd)
+      mux <- apply(x[index, ], 2, mean)
+      sigx <- apply(x[index, ], 2, sd)
     }
   }
-  xnor <- scale(x,mux,sigx)
-  if(family=="binomial"){
-    ycen <- y}
-  else if(family=="gaussian"){
-    ycen <- scale(y,muy,FALSE)}
-  return(list(xnor=xnor,ycen=ycen,mux=mux,sigx=sigx,muy=muy))
+  # Effectively scaling X
+  xnor <- scale(x, mux, sigx)
+  # NOTE: x is being scaled as a whole, so the returning X has the same sample size as the input X!
+  
+  if (family == "binomial") {
+    ycen <- y
+    } else if (family == "gaussian") {
+    ycen <- scale(y, muy, FALSE)
+  }
+  
+  
+  return(list(xnor = xnor,
+              ycen = ycen, 
+              mux = mux, 
+              sigx = sigx,
+              muy = muy))
 }
