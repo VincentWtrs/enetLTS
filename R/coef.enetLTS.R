47011a424b12d20622e9a82c8545bb9022a3732e
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-04-03 21:12:19 +0200

    layout

--- R/coef.enetLTS.R
+++ R/coef.enetLTS.R
@@ -1,12 +1,15 @@
 coef.enetLTS <- function(object, vers=c("reweighted","raw"), zeros = TRUE, ...){
    vers <- match.arg(vers)
-   nbeta <- predict.enetLTS(object, newX = object$inputs$xx, vers=vers, type = "coefficients", ...)
+   nbeta <- predict.enetLTS(object, 
+                            newX = object$inputs$xx, 
+                            vers = vers, 
+                            type = "coefficients", ...)
    nbeta <- as.numeric(unlist(nbeta))
-   if(isTRUE(zeros)){
+   if (isTRUE(zeros)) {
       nbeta <- nbeta
       #names(nbeta) <- 1:length(nbeta)
       names(nbeta) <- 0:length(nbeta) # Changing to start from X0
-   }else if(!isTRUE(zeros)){
+   } else if (!isTRUE(zeros)) {
       namesbeta <- which(nbeta != 0)
       nbeta <- nbeta[nbeta != 0]
       names(nbeta) <- namesbeta
