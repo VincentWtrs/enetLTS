969d705a538c4c6b1a3723943f5e6a8d57b752c0
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-04-03 21:12:32 +0200

    debugging

--- R/print.enetLTS.R
+++ R/print.enetLTS.R
@@ -8,16 +8,18 @@ print.enetLTS <- function(x, vers = c("reweighted", "raw"), zeros = FALSE, ...){
 
       cat("\nCall: ", deparse(x$call), "\n\n")
 
-      coefficients <- coef.enetLTS(x,vers=vers,zeros=zeros)
-      cat("\nCoefficients:\n")
+      coefficients <- coef.enetLTS(x, 
+                                   vers = vers,
+                                   zeros = zeros)
+      cat("\nCoefficients blablabla:\n")
       print(coefficients,...)
 
-      nCoefficients <- sum(coefficients!=0)
+      nCoefficients <- sum(coefficients != 0)
       cat("\n number of the nonzero coefficients:\n")
       print(nCoefficients)
 
-      cat(paste("\n alpha:",x$alpha))
-      cat(paste("\n lambda:",x$lambda))
-      cat(paste("\n lambdaw:",x$lambdaw))
+      cat(paste("\n alpha:", x$alpha))
+      cat(paste("\n lambda:", x$lambda))
+      cat(paste("\n lambdaw:", x$lambdaw))
 
 }
