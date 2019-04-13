87784dfc1bdb11c31bf798c1e37bdbcc58d4e06a
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-04-13 16:30:18 +0200

    separated twoCstep into an .R file

---
+++ R/twoCstep.R
@@ -0,0 +1,49 @@
+twoCstep <- function(c, x, y, family, h, hsize, alpha, lambda) {
+
+  ## C-step 1
+  # Case: Binomial
+  if (family=="binomial") {
+    Cstep1 <- CStep(x = x,
+                    y = y,
+                    family = family,
+                    indx = index.subsets[, c],
+                    h = h,
+                    hsize = hsize,
+                    alpha = alpha,
+                    lambda = lambda/4,
+                    scal = FALSE)
+    
+  # Case: Gaussian
+  } else if (family=="gaussian") {
+    Cstep1 <- CStep(x = x,
+                    y = y,
+                    family = family,
+                    indx = index.subsets[, c],
+                    h = h,
+                    hsize = hsize,
+                    alpha = alpha,
+                    lambda = lambda/3,
+                    scal = FALSE)
+  }
+  
+  # Gathering results from C-step 1
+  indx1 <- Cstep1$index
+  object1 <- Cstep1$object
+  
+  ## C-step 2 (Same for both cases)
+  Cstep2 <- CStep(x = x,
+                  y = y,
+                  family = family,
+                  indx = indx1,
+                  h = h,
+                  hsize = hsize,
+                  alpha = alpha,
+                  lambda = lambda/h, # h observations
+                  scal = scal) 
+  
+  # Gathering results from C-step 2
+  indx2 <- Cstep2$index
+  object <- Cstep2$object
+  
+  return(list(obj = object, indx = indx2))
+}
