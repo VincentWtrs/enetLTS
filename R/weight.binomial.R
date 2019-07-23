df522d4c4844f659ec69eaf8ed6b50726a04964b
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-07-23 15:28:32 +0200

    debugging prints

--- R/weight.binomial.R
+++ R/weight.binomial.R
@@ -3,12 +3,15 @@ weight.binomial <- function(x, y, beta, intercept, del){
   print("Dimension of cbind(1, x):")
   print(dim(cbind(1, x)))
   
+  
   print("Dimension of beta:")
   print(dim(beta))
   
+  print("Printing structure of beta:")
+  print(str(beta))
+  
   print("Head of x[, 1:8]")
   print(head(x[, 1:8]))
-  
   
   if(intercept) {
     pi <- exp(cbind(1, x) %*% beta)/(1 + exp(cbind(1, x) %*% beta))
