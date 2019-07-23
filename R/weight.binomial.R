f65fcf9a0db13f66f490031a995ead0fe168a5fb
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-07-23 15:12:28 +0200

    debug statements to check conformality issues on matrices

--- R/weight.binomial.R
+++ R/weight.binomial.R
@@ -1,4 +1,10 @@
 weight.binomial <- function(x, y, beta, intercept, del){
+  # TODO REMOVE DEBUG
+  print(dim(cbind(1, x)))
+  print(beta)
+  print(head(x))
+  
+  
   if(intercept) {
     pi <- exp(cbind(1, x) %*% beta)/(1 + exp(cbind(1, x) %*% beta))
     res <- (y - pi) / sqrt(pi*(1-pi))
