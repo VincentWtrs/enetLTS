8bab5cb28c76e109dcfc29b521965a45c96201c8
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-08-06 21:08:49 +0200

    removed old comments, improved intercept handling

--- R/logit_df.R
+++ R/logit_df.R
@@ -1,4 +1,4 @@
-logit_df <- function(model, X, alpha = NULL, intercept, s = NULL){
+logit_df2 <- function(model, X, alpha = NULL, s = NULL){
   ### logit_df() FUNCTION: calculating the effective degrees of freedom in a potentially penalized logistic regression using hat matrix trace
   
   ## INPUTS
@@ -17,18 +17,40 @@ logit_df <- function(model, X, alpha = NULL, intercept, s = NULL){
   # 3. IDEA to work with the intercept is to see in coefficients(model) is it's 0, if yes: no intercept fitted, becuase it's always unpenalized!
   # 4. I was afraid this might break down at the matrix inversion when |Active set| > n, but seems to be okay!
   
-  if(length(model$lambda) > 1){
-    if(is.null(s)){
-      stop("Multiple models were provided but no lambda (s) argument was given.")
+  detect_intercept <- function(model) {
+    if(any(model$a0 !=  0)){
+      intercept <- TRUE
+    } else {
+      intercept <- FALSE
+    }
+    # OUTPUT
+    return(intercept)
+  }
+  
+  detect_regularization_path <- function(model) {
+    if(length(model$lambda) > 1){
+      path <- TRUE
+    } else {
+      path <- FALSE
     }
   }
   
-  # Setting lambda <- s if given regularization path
-  if(length(model$lambda) > 1){
-    lambda <- s
-  }
-  if(length(model$lambda) == 1){
-    lambda <- model$lambda
+  # Checking if the model was fitted with an intercept
+  intercept <- detect_intercept(model)
+  
+  # Checking if the model was fitted over a regularization path
+  path <- detect_regularization_path(model)
+  
+  # Checking, if there was a regularization path fitted that an additional single labmda is supplied and then passing on that lambda if so
+  if(path) {
+    if(is.null(s)){
+      stop("logit_df was supplied a glmnet model fitted over a regularization path but no specific lambda was given to calculate the degrees of freedom")
+    } else {
+      # Setting the supplied lambda as the lambda to further use
+      if(length(s) == 1){ # Additional check, we can only have a single lambda here
+        lambda <- s
+      }
+    }
   }
   
   # Trying to retrieve alpha if missing in function call
@@ -40,118 +62,102 @@ logit_df <- function(model, X, alpha = NULL, intercept, s = NULL){
     }
   }
   
-  # TO DO: CHANGE BY CODE BELOW (model.matrix())
-  # If the model was estimated using an intercept, the matrix calculations need to have a 1 in the column as well!
-  if(intercept == TRUE){
+  ## Construction of the design matrix
+  # If there is an intercept, add a first column of 1
+  if(intercept) {
     X <- cbind(1, X)
-    colnames(X)[1] <- "X0" # Needs to be colnames because matrix!
+    colnames(X)[1] <- "X0"
+  } # If there is no intercept, the X matrix is good as is
+  
+  ## Extracting fitted beta coefficients
+  # If intercept, get the beta0 in there as well!
+  if(intercept) {
+    beta <- as.matrix(coefficients(model, s = lambda)) # NOT doing [-1, ] to remove intercept!
+    # NOTE: Even if there is a single model fitted, we can still request the coefficients() method validly with the s = lambda argument!
+  }
+  if(!intercept) {
+    beta <- as.matrix(coefficients(model, s = lambda))[-1, ] # DOING [-1, ], note that coefficients will always provide an intercept...
   }
   
-  # ALTERNATIVE TO INTERCEPT HASSLE (TO DO: TEST)
-  #X <- model.matrix(model)
-
-  # Extracting fitted beta (coefficients)
-  if(intercept == TRUE){
-    coefs <- as.matrix(coefficients(model))
+  ## Extracting the active set
+  active_set <- which(beta != 0)
+  
+  # Taking intercept into account
+  if(intercept) {
+    active_set_noint <- active_set[-1]
   }
-  if(intercept == FALSE){
-    coefs <- as.matrix(coefficients(model))[-1, ] # EXCLUDING intercept, as.matrix() to get rid of sparse matrix formatting
-    # Alternatively to get rid of intercept: model$beta
+  if(!intercept) {
+    active_set_noint <- active_set  # Then it's just the active set itself
   }
-
-  # Extracting the active subset
-  active_set <- which(coefs != 0) # Nonzero coefs INDICES (Note they start from 1, not 0)
   
-  # TODO (REMOVE: DEBUG)
-  print("the active set is:")
-  print(active_set)
-  
-  print("the dimension of X_active")
-  print(dim(X_active))
-  
-  X_active <- X[, active_set] # X subsetted with above indices
-  
-  # Extracting predictions
-  p_hat <- as.numeric(predict(object = model,
-                              newx = X,
-                              type = "response")) 
-  # Forcing as.numeric() because diag(p_hat * (1 - p_hat)) not as nice with matrices
-  
-  ### Calculating degrees of freedom
-  ## If alpha = 1 (LASSO): Trivial
-  if(alpha == 1){
-    df <- model$df # TO DO: check with intercept, make sure it's consistent
-  } 
-  ## If alpha != 0: Calculate
-  if(alpha != 1){
-  ## Calculating Hat matrix (H)
-  
-  # Getting lambda
-  #lambda <- model$lambda
+  # If the active set if not empty:
+  if(length(active_set_noint) != 0) {
+    # Constructing the matrix with only active columns X_active
+    X_active <- X[, active_set]
+    X_active_noint <- X[, active_set_noint]
     
-  # Weight matrix W (diagonal), see GLM theory!
-  W <- diag(p_hat * (1 - p_hat))
+    ## Producing predictions
+    # Making specific matrix for predictions
+    if(intercept) {
+      X_noint <- as.matrix(X[, -1]) # Not necessary for predict() function
+    }
+    if(!intercept) {
+      X_noint <- as.matrix(X)
+    }
+    
+    # Predicting
+    p_hat <- as.numeric(predict(object = model,
+                                newx = X_noint,
+                                type = "response",
+                                s = lambda)) # NEW # TODO REMOVE (TEMP)
+    # Forcing as.numeric() because diag(p_hat * (1 - p_hat)) not as nice with matrices
+    
+    ### Calculating degrees of freedom
+    ## If alpha = 1 (LASSO): Trivial
+    if(alpha == 1){
+      df <- sum(beta != 0) # Amount of nonzero coefficients
+    } 
+    ## If alpha != 0: Calculate
+    if(alpha != 1){
+      ## Calculating Hat matrix (H)
+      # Weight matrix W (diagonal), see GLM theory!
+      W <- diag(p_hat * (1 - p_hat))
+      
+      # Calculating Hat (H) Matrix
+      # NEW: Trying to fix wrong regularization
+      I <- diag(length(active_set_noint))
+      #H <- sqrt(W) %*% X_active %*% solve((t(X_active) %*% W %*% X_active)  + (lambda * (1 - alpha)/2) * diag(length(active_set))) %*% t(X_active) %*% sqrt(W)
+      H <- sqrt(W) %*% X_active_noint %*% solve((t(X_active_noint) %*% W %*% X_active_noint)  + (lambda * (1 - alpha)/2) * I) %*% t(X_active_noint) %*% sqrt(W)  # NEW
+      
+      
+      # Formula: W^(1/2) X_a (X'_a W X_a + lambda_2 * I)^-1 X'_a W^(1/2) (Yes transpose W, but it's diagonal, so it doesn't matter)
+      # Note: lambda_2 = lambda (1 - alpha)/2 here
+      
+      # Degrees of freedom = trace of H
+      H_trace <- sum(diag(H))
+      
+      # ADDING THE INTERCEPT AGAIN (UNREGULARIZED SO 1 EXTRA DF)
+      if(intercept) {
+        df <- 1 + H_trace
+      }
+      if(!intercept) {
+        df <- H_trace
+      }
+    }
+  }
   
-  # Calculating Hat (H) Matrix
-  H <- sqrt(W) %*% X_active %*% solve((t(X_active) %*% W %*% X_active)  + (lambda * (1 - alpha)/2) * diag(length(active_set))) %*% t(X_active) %*% sqrt(W)
-  # Formula: W^(1/2) X_a (X'_a W X_a + lambda_2 * I)^-1 X'_a W^(1/2) (Yes transpose W, but it's diagonal, so it doesn't matter)
-  # Note: lambda_2 = lambda (1 - alpha)/2 here
-  
-  # Degrees of freedom = trace of H
-  H_trace <- sum(diag(H))
-  df <- H_trace
+  # If the active set length (without intercept) == 0
+  if(length(active_set_noint) == 0) {
+    if(intercept) {
+      df <- 1
+    } else if(!intercept){
+      df <- 0
+    }
   }
   
   # OUTPUT
   return(df)
 }
-   
 
-  
 
-# ##################################### OLD AND QUITE POSSIBLY WRONG BECAUSE ONE WORKS WITH GLMNET IN THE FITTING PROCEDURE NOT WITH ENETLTS ########
-#   ### Extracting some parameters
-#   ## enetLTS models
-#   if(class(model) == "enetLTS"){
-#     # Predictions
-#     p_hat <- model$fitted.values # These are the reweigted fitted values!
-#     
-#     # Coefficients
-#     coefs <- coefficients(model) # Gets coefficients (INCLUDING INTERCEPT)
-#     coefs_nonzero <- coefs[coefs != 0]
-#     index_coefs_nonzero <- as.numeric(names(coefs_nonzero)) # COLUMN NUMBERS of nonzero coefs
-#     # Note the column numbers start from 1! and e.g. go to 51
-#     
-#     ## Getting subset out
-#     wt <- model$raw.wt == 1
-#     
-#     X <- X[wt, ]
-#     p_hat <- p_hat[wt]
-#     
-#     
-#     
-#     # Alpha, Lambda(reweighted)
-#     lambda <- model$lambdaw
-#     alpha <- model$alpha
-#   } else {  ## Other kind of models
-#     lambda <- model$lambda # UNDER THE ASSUMPTION ONLY A SINGLE LAMBDA IS GIVEN
-#     p_hat <- predict(object = model, type = "response", newx = X[, -1]) # Without intercept fitting
-#     
-#   }
-#   
-#   # Extracting the active set
-#   X <- X[, index_coefs_nonzero] # WITH OR WITHOUT INTERCEPT?!
-#   
-#   # Calculating W matrix
-#   W <- diag(p_hat * (1 - p_hat))
-#   
-#   # Amount of obs
-#   nobs <- length(p_hat)
-#   
-#   # Calculating Hat (H) Matrix
-#   H <- sqrt(W) %*% X %*% solve((t(X) %*% W %*% X)  + lambda * (1 - alpha)/2) %*% t(X) %*% sqrt(W)
-#   
-#   # Calculating df 
-#   H_trace <- sum(diag(H))
-  
 
