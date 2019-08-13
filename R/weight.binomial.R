d6d2911eae3d8db895b79f6115347bc277038dc5
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-08-14 00:53:17 +0200

    removed temp debugs

--- R/weight.binomial.R
+++ R/weight.binomial.R
@@ -1,17 +1,4 @@
 weight.binomial <- function(x, y, beta, intercept, del){
-  # TODO REMOVE DEBUG
-  print("Dimension of cbind(1, x):")
-  print(dim(cbind(1, x)))
-  
-  
-  print("Dimension of beta:")
-  print(dim(beta))
-  
-  print("Printing structure of beta:")
-  print(str(beta))
-  
-  print("Head of x[, 1:8]")
-  print(head(x[, 1:8]))
   
   if(intercept) {
     pi <- exp(cbind(1, x) %*% beta)/(1 + exp(cbind(1, x) %*% beta))
