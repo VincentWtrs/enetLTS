2578e2a074f6c4d76db601d0bba78b9f72222512
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-08-14 22:12:45 +0200

    added additional protection against NA values...

--- R/weight.binomial.R
+++ R/weight.binomial.R
@@ -1,12 +1,46 @@
-weight.binomial <- function(x, y, beta, intercept, del){
+weight.binomial <- function(x, y, beta, intercept, del) {
   
+  ## This function gives 0/1 weights based on the Pearson residuals, which are assumed to be approximately standard normal.S
+  
+  ## Case: Intercept (Always for logistic regression)
   if(intercept) {
-    pi <- exp(cbind(1, x) %*% beta)/(1 + exp(cbind(1, x) %*% beta))
+    # Calculating predicted probabilities
+    pi <- exp(cbind(1, x) %*% beta) / (1 + exp(cbind(1, x) %*% beta))
+    
+    # Calculating Pearson Residual (Non-studentized)
     res <- (y - pi) / sqrt(pi*(1-pi))
-  } else{
+    
+    ## Catching some NAs for very high predicted probabilities
+    # Catching the indices that are NA
+    res_na_indx <- which(is.na(res))
+    
+    # Catching indices that are very sure of their predictions and are CORRECT
+    correct_sure_indx <- which(y - pi < 0.000000001)
+    
+    # Intersection of these both
+    intersection_na_sure <- intersect(res_na_indx, correct_sure_indx)
+    
+    # Assigning 0 to these (we are very sure and correct!)
+    res[res_intersection_na_sure] <- 0
+    
+    # Taking difference with the sure ones
+    incorrect_sure_indx <- setdiff(res_na_indx, correct_sure_indx)
+    
+    # Setting those incorrect sure ones rather big
+    print("The residuals before replacing had the following y - pi_hat values:")
+    print(y[incorrect_sure_indx] - pi[incorrect_sure_indx])
+    res[incorrect_sure_indx] <- 3 # 3 Standard deviations away so will be flagged
+    
+  ## Case: No intercept 
+  } else {
+    
+    # Calculating predicted probabilities
     pi <- exp(x %*% beta[-1])/(1 + exp(x %*% beta[-1]))
     res <- (y - pi) / sqrt(pi * (1 - pi))
   }
+  
+  
   we <- as.integer(abs(res) <= qnorm(1 - del))
+  
   return(we)
 }
