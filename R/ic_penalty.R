5ef430ac9ef290eb8947868de3e1cc42215c9c7e
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-08-15 06:43:45 +0200

    corrected ?check on HBIC

--- R/ic_penalty.R
+++ R/ic_penalty.R
@@ -116,7 +116,7 @@ ic_penalty <- function(type, model, X, alpha, EBIC_sigma = 0.25, s = NULL, inter
   # HBIC: High-dimensional BIC
   if(type == "HBIC"){
     sigma <- 1.5 # Empirically well-performing ]1, 2]
-    if(sigma > 1){
+    if(sigma < 1){ # TODO CHECK
       stop("Sigma is required to be > 1 following Wang & Zhu (2011)")
     }
     penalty <- (2 * sigma * df * log(p))/nobs
