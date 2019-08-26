8858018d2dd8bb08e05bec3da8b5b3884df03b97
VincentWtrs <VincentWtrs@users.noreply.github.com>
2019-08-26 22:22:03 +0200

    cleaned code and added shortcut lasso option

--- R/logit_df.R
+++ R/logit_df.R
@@ -1,4 +1,4 @@
-logit_df <- function(model, X, alpha = NULL, lambda = NULL){
+logit_df <- function(model, X, alpha = NULL, lambda = NULL, lasso_shortcut = TRUE){
   ### logit_df() FUNCTION: calculating the effective degrees of freedom in a potentially penalized logistic regression using hat matrix trace
   
   ## INPUTS
@@ -17,9 +17,12 @@ logit_df <- function(model, X, alpha = NULL, lambda = NULL){
   # 3. IDEA to work with the intercept is to see in coefficients(model) is it's 0, if yes: no intercept fitted, becuase it's always unpenalized!
   # 4. I was afraid this might break down at the matrix inversion when |Active set| > n, but seems to be okay!
   
+  ## TODO
+  # 1. Extend the function to glm_df to open up options for Poisson e.g. 
+  
   ## Defining detect intercept function
   detect_intercept <- function(model) {
-    if(any(model$a0 !=  0)){
+    if(any(model$a0 !=  0)) {
       intercept <- TRUE
     } else {
       intercept <- FALSE
@@ -37,15 +40,10 @@ logit_df <- function(model, X, alpha = NULL, lambda = NULL){
     }
   }
   
-  # Checking if the model was fitted with an intercept
-  intercept <- detect_intercept(model)
-  
-  # Checking if the model was fitted over a regularization path
-  path <- detect_regularization_path(model)
-  
-  # Checking, if there was a regularization path fitted that an additional single labmda is supplied and then passing on that lambda if so
-  if(path) {
-    if(is.null(s)){
+  ## Error handling
+  # Checking regularization path consistency with supplied lambda
+  if (isTRUE(path)) {
+    if (is.null(lambda)) {
       stop("logit_df was supplied a glmnet model fitted over a regularization path but no specific lambda was given to calculate the degrees of freedom")
     } else {
       # Setting the supplied lambda as the lambda to further use
@@ -54,30 +52,39 @@ logit_df <- function(model, X, alpha = NULL, lambda = NULL){
       }
     }
   }
+  # TODO MAYBE THIS SHOULD BE MOVED IN WITH THE DETECT INTERCEPT FUNCTION
+  
   
   # Trying to retrieve alpha if missing in function call
-  if(missing(alpha)){
-    if(is.numeric(model$call$alpha)){
+  if (missing(alpha)) {
+    if(is.numeric(model$call$alpha)){ # If the function was called with explicit numeric value it can retrieve a meaningfull alpha
       alpha <- model$call$alpha # Trying to extract from the function call if possible
-    } else { # But if it's another variable, it will return e.g. "myalpha" and will not be of any use
+    } else { # But if it's another variable, it will return e.g. "my_alpha" and will not be of any use
       stop("No alpha value given for logit_df(), also no value could be succesfully extracted from the model object, supply a valid alpha: [0, 1]")
     }
   }
   
+  # Checking if the model was fitted with an intercept
+  intercept <- detect_intercept(model)
+  
+  # Checking if the model was fitted over a regularization path
+  path <- detect_regularization_path(model)
+  
+
   ## Construction of the design matrix
   # If there is an intercept, add a first column of 1
-  if(intercept) {
+  if(isTRUE(intercept)) {
     X <- cbind(1, X)
     colnames(X)[1] <- "X0"
   } # If there is no intercept, the X matrix is good as is
   
   ## Extracting fitted beta coefficients
   # If intercept, get the beta0 in there as well!
-  if(intercept) {
+  if (isTRUE(intercept)) {
     beta <- as.matrix(coefficients(model, s = lambda)) # NOT doing [-1, ] to remove intercept!
     # NOTE: Even if there is a single model fitted, we can still request the coefficients() method validly with the s = lambda argument!
   }
-  if(!intercept) {
+  if (isFALSE(intercept)) {
     beta <- as.matrix(coefficients(model, s = lambda))[-1, ] # DOING [-1, ], note that coefficients will always provide an intercept...
   }
   
@@ -85,25 +92,26 @@ logit_df <- function(model, X, alpha = NULL, lambda = NULL){
   active_set <- which(beta != 0)
   
   # Taking intercept into account
-  if(intercept) {
-    active_set_noint <- active_set[-1]
+  if(isTRUE(intercept)) {
+    active_set_noint <- active_set[-1] # Removing first element (intercept)
   }
-  if(!intercept) {
+  if(isFALSE(intercept)) {
     active_set_noint <- active_set  # Then it's just the active set itself
   }
   
   # If the active set if not empty:
   if(length(active_set_noint) != 0) {
+    
     # Constructing the matrix with only active columns X_active
     X_active <- X[, active_set]
     X_active_noint <- X[, active_set_noint]
     
     ## Producing predictions
-    # Making specific matrix for predictions
-    if(intercept) {
-      X_noint <- as.matrix(X[, -1]) # Not necessary for predict() function
+    # NOTE: Since predict() will handle the active set itself properly we just need to make sure we give the full matrix but WITHOUT CONSTANT COLUMN!
+    if (isTRUE(intercept)) {
+      X_noint <- as.matrix(X[, -1]) # Removing the constant column from the X matrix which is (1, X)
     }
-    if(!intercept) {
+    if (isFALSE(intercept)) {
       X_noint <- as.matrix(X)
     }
     
@@ -111,38 +119,43 @@ logit_df <- function(model, X, alpha = NULL, lambda = NULL){
     p_hat <- as.numeric(predict(object = model,
                                 newx = X_noint,
                                 type = "response",
-                                s = lambda)) # NEW # TODO REMOVE (TEMP)
-    # Forcing as.numeric() because diag(p_hat * (1 - p_hat)) not as nice with matrices
+                                s = lambda))
     
     ### Calculating degrees of freedom
-    ## If alpha = 1 (LASSO): Trivial
-    if(alpha == 1){
-      df <- sum(beta != 0) # Amount of nonzero coefficients
-    } 
-    ## If alpha != 0: Calculate
-    if(alpha != 1){
+    ## If alpha = 1 (LASSO): Shortcut calculation
+    if(isTRUE(lasso_shortcut)) {
+      if(alpha == 1) { # Amount of nonzero coefficients
+        df <- sum(beta != 0)
+      }
+    }
+    ## If alpha != 0: Calculate using weighted hat matrix
+    if (alpha != 1) {
       ## Calculating Hat matrix (H)
       # Weight matrix W (diagonal), see GLM theory!
+      # NOTE: SINCE NO REGULARIZATION IS APPLIED ON THE INTERCEPT: EXCLUDE FROM CALCULATION AND ADD AT THE END
       W <- diag(p_hat * (1 - p_hat))
+      W_sqrt <- diag(sqrt(p_hat) * (1 - sqrt(p_hat))) # Making square root beforehand (faster?)
       
       # Calculating Hat (H) Matrix
       # NEW: Trying to fix wrong regularization
       I <- diag(length(active_set_noint))
       #H <- sqrt(W) %*% X_active %*% solve((t(X_active) %*% W %*% X_active)  + (lambda * (1 - alpha)/2) * diag(length(active_set))) %*% t(X_active) %*% sqrt(W)
-      H <- sqrt(W) %*% X_active_noint %*% solve((t(X_active_noint) %*% W %*% X_active_noint)  + (lambda * (1 - alpha)/2) * I) %*% t(X_active_noint) %*% sqrt(W)  # NEW
+      H <- W_sqrt %*% X_active_noint %*% solve((t(X_active_noint) %*% W %*% X_active_noint)  + (lambda * (1 - alpha)/2) * I) %*% t(X_active_noint) %*% W_sqrt  # NEW
       
-      
-      # Formula: W^(1/2) X_a (X'_a W X_a + lambda_2 * I)^-1 X'_a W^(1/2) (Yes transpose W, but it's diagonal, so it doesn't matter)
+
+      # Formula: W^(1/2) X_a (X'_a W X_a + lambda_2 * I)^-1 X'_a W^(1/2) (Yes transpose W at the end, but it's diagonal, so it doesn't matter)
       # Note: lambda_2 = lambda (1 - alpha)/2 here
+      
+      # TODO CHECK IF THE FORMULA IS CORRECT FOR THE IMPLEMENTATION IN GLMNET (CHECK: I THINK SO)
       
       # Degrees of freedom = trace of H
       H_trace <- sum(diag(H))
       
       # ADDING THE INTERCEPT AGAIN (UNREGULARIZED SO 1 EXTRA DF)
-      if(intercept) {
+      if(isTRUE(intercept)) {
         df <- 1 + H_trace
       }
-      if(!intercept) {
+      if(isFALSE(intercept)) {
         df <- H_trace
       }
     }
